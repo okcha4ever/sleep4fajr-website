fe02c217a0880d5e6aff7b79a76f0cdf4e0a7054
Amine <minoukhiari4@gmail.com>
2026-05-02 00:44:01 +0100

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -57,6 +58,7 @@ export default function RootLayout({
             Amine Khiari
           </a>
         </footer>
+        <Analytics />
       </body>
     </html>
   );
